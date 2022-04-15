eb63ef66bd50c6f36e479d75b22812529b876a2c
Griffin Moyer <moyerga@outlook.com>
2022-04-15 09:27:44 -0500

    My first version of my aquarium code

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// Create objects needed for this code - fishCollection, travelLocations, tankTips
+
+const fishCollection = [
+    {
+        id: 1,
+        name: "Jacob",
+        imageLink: "",
+        species: ,
+        length: ,
+        location: "Costa Rica",
+        diet: "food"
+    }
+]
